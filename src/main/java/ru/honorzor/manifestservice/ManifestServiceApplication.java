adcfabab8f237bfee7c5c0ea675b5822195ea889
ioannj <itrapeznikov@alfabank.ru>
2022-04-12 02:00:00 +0300

    added eureka client

--- src/main/java/ru/honorzor/manifestservice/ManifestServiceApplication.java
+++ src/main/java/ru/honorzor/manifestservice/ManifestServiceApplication.java
@@ -2,8 +2,10 @@ package ru.honorzor.manifestservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ManifestServiceApplication {
 
     public static void main(String[] args) {
